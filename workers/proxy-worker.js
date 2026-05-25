53c752740a1320c5433ec71797187cf18a797164
Natalie Spiva <spivajohnathan64@gmail.com>
2026-05-25 14:55:54 -0700

    Add Cloudflare Worker proxy + deploy workflow

---
+++ workers/proxy-worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url)
+    const target = `https://spivanatalie64.github.io/acreetionos-blog${url.pathname}${url.search}`
+    const req = new Request(target, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    })
+    req.headers.set('Host', 'spivanatalie64.github.io')
+    return fetch(req)
+  },
+}
