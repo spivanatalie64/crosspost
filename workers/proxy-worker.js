7fb8c5e2479cae05eacc7490c6db762a67c8cb83
Natalie Spiva <spivajohnathan64@gmail.com>
2026-05-25 15:07:21 -0700

    Use Service Worker format for Cloudflare compatibility

--- workers/proxy-worker.js
+++ workers/proxy-worker.js
@@ -1,13 +1,15 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url)
-    const target = `https://spivanatalie64.github.io/acreetionos-blog${url.pathname}${url.search}`
-    const req = new Request(target, {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-    })
-    req.headers.set('Host', 'spivanatalie64.github.io')
-    return fetch(req)
-  },
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  const target = `https://spivanatalie64.github.io/acreetionos-blog${url.pathname}${url.search}`
+  const req = new Request(target, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  })
+  req.headers.set('Host', 'spivanatalie64.github.io')
+  return fetch(req)
 }
